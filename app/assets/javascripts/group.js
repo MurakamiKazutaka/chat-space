6dd6de0787bfc5c6a8d72c4ab4aa95d9764e3c4c
M.Kazu <55425356+MurakamiKazutaka@users.noreply.github.com>
2019-10-28 07:56:24 +0900

    git_deploy

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -8,7 +8,7 @@ $(document).on('turbolinks:load', function(){
     var html =
       `<div class="chat-group-user clearfix">
           <p class="chat-group-user__name">${ user.name }</p>
-          <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name=${ user.name }>追加</a>
+          <a id="chat-group-user__btn--add" class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name=${ user.name }>追加</a>
       </div>`
     search_list.append(html);
     return html;
@@ -79,14 +79,14 @@ $(document).on('turbolinks:load', function(){
       resetSearchUserResult();
     }
 
-  $('.chat-group-user').on('click', '.chat-group-user__btn--add', function() {
+  $(document).on('click', '#chat-group-user__btn--add', function() {
     var name = $(this).data("user-name");
     var user_id = $(this).data("user-id");
     $(this).parent().remove();
     appendUserToMemberList(name, user_id);
   });
 
-  $('.chat-group-user').on("click", '.user-search-remove', function() {
+  $(document).on("click", '.user-search-remove', function() {
     $(this).parent().remove();
   });
   });
